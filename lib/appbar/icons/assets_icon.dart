82ff65ea0ccdf8226da60fd1be803b8fd35d9295
Badrakova Valeria <lera_18a@mail.ru>
2025-09-29 16:05:41 +0300

    add appbar_icons

---
+++ lib/appbar/icons/assets_icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AssetsIcon extends StatelessWidget {
+  const AssetsIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: () {}, icon: Icon(Icons.settings));
+  }
+}
