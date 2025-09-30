c8fb23547e70d27355d859a8b25e7e5a72cbe238
Badrakova Valeria <lera_18a@mail.ru>
2025-09-30 17:05:00 +0300

    add the icon of project

--- lib/ui/theme/theme_settings_page.dart
+++ lib/ui/theme/theme_settings_page.dart
@@ -1,6 +1,5 @@
 import 'package:dio_http_example/ui/theme/theme_models/switch_model.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ThemeSettingsPage extends StatefulWidget {
   const ThemeSettingsPage({
