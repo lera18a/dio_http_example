44a5ecf7affe1901015c920495d968914f126431
Badrakova Valeria <lera_18a@mail.ru>
2025-10-04 14:32:17 +0300

    moved MultiBlocProvider under MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
-import 'package:dio_http_example/bloc/bloc_holidays/holidays_bloc.dart';
-import 'package:dio_http_example/bloc/bloc_theme/theme_bloc.dart';
 import 'package:dio_http_example/my_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => HolidaysBloc()),
-        BlocProvider(create: (context) => ThemeBloc()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:dio_http_example/bloc/bloc_holidays/holidays_bloc.dart';
 import 'package:dio_http_example/bloc/bloc_theme/theme_bloc.dart';
 import 'package:dio_http_example/ui/splash_screen.dart';
 import 'package:dio_http_example/ui/theme/theme.dart';
@@ -14,15 +15,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeBloc, ThemeState>(
-      builder: (context, state) {
-        return MaterialApp(
-          darkTheme: darkTheme,
-          theme: lightTheme,
-          themeMode: state.isLight ? ThemeMode.light : ThemeMode.dark,
-          home: SplashScreen(),
-        );
-      },
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => HolidaysBloc()),
+        BlocProvider(create: (context) => ThemeBloc()),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          return MaterialApp(
+            darkTheme: darkTheme,
+            theme: lightTheme,
+            themeMode: state.isLight ? ThemeMode.light : ThemeMode.dark,
+            home: SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
