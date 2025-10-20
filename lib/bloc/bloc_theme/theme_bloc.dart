e265052f6e13d716acd7231e788d13cf6cfc8fb4
Badrakova Valeria <lera_18a@mail.ru>
2025-10-20 18:47:54 +0300

    add ThemeInitEvent

--- lib/bloc/bloc_theme/theme_bloc.dart
+++ lib/bloc/bloc_theme/theme_bloc.dart
@@ -1,14 +1,30 @@
 import 'package:bloc/bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeBloc() : super(ThemeState(isLight: true)) {
+  ThemeBloc(this.prefs) : super(ThemeState(isLight: true)) {
     on<ToggleThemeEvent>(_onToggleThemeEvent);
+    on<ThemeInitEvent>(_onThemeInitEvent);
   }
 
-  void _onToggleThemeEvent(ToggleThemeEvent event, Emitter<ThemeState> emit) {
-    emit(ThemeState(isLight: !state.isLight));
+  final SharedPreferences prefs;
+  static const _themeKey = 'theme';
+
+  Future<void> _onToggleThemeEvent(
+    ToggleThemeEvent event,
+    Emitter<ThemeState> emit,
+  ) async {
+    final finalState = !state.isLight;
+    emit(ThemeState(isLight: finalState));
+    await prefs.setBool(_themeKey, finalState);
+  }
+
+  void _onThemeInitEvent(ThemeInitEvent event, Emitter<ThemeState> emit) {
+    final saved = prefs.getBool(_themeKey);
+    if (saved == null || saved == state.isLight) return;
+    emit(ThemeState(isLight: saved));
   }
 }
--- lib/bloc/bloc_theme/theme_event.dart
+++ lib/bloc/bloc_theme/theme_event.dart
@@ -3,3 +3,5 @@ part of 'theme_bloc.dart';
 sealed class ThemeEvent {}
 
 class ToggleThemeEvent extends ThemeEvent {}
+
+class ThemeInitEvent extends ThemeEvent {}
