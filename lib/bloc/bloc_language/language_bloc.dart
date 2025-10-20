d4ec30a5b9babdd6e9f8324a54d03561133cc2ad
Badrakova Valeria <lera_18a@mail.ru>
2025-10-20 18:47:30 +0300

    add LanguageInitEvent

--- lib/bloc/bloc_language/language_bloc.dart
+++ lib/bloc/bloc_language/language_bloc.dart
@@ -1,21 +1,36 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'language_event.dart';
 part 'language_state.dart';
 
 class LanguageBloc extends Bloc<LanguageEvent, LanguageState> {
-  LanguageBloc() : super(LanguageState(local: Locale('ru'))) {
+  LanguageBloc(this.prefs) : super(LanguageState(local: Locale('ru'))) {
     on<ToggleLanguageEvent>(_onToggleLanguageEvent);
+    on<LanguageInitEvent>(_onLanguageInitEvent);
   }
+  static const _langKey = 'language';
+  final SharedPreferences prefs;
 
-  void _onToggleLanguageEvent(
+  Future<void> _onToggleLanguageEvent(
     ToggleLanguageEvent event,
     Emitter<LanguageState> emit,
-  ) {
+  ) async {
     final locate = state.local.languageCode == 'en'
         ? const Locale('ru')
         : const Locale('en');
     emit(LanguageState(local: locate));
+    await prefs.setString(_langKey, locate.languageCode);
+  }
+
+  void _onLanguageInitEvent(
+    LanguageInitEvent event,
+    Emitter<LanguageState> emit,
+  ) {
+    final saved = prefs.getString(_langKey);
+    if (saved != null && saved.isNotEmpty) {
+      emit(LanguageState(local: Locale(saved)));
+    }
   }
 }
--- lib/bloc/bloc_language/language_event.dart
+++ lib/bloc/bloc_language/language_event.dart
@@ -3,3 +3,5 @@ part of 'language_bloc.dart';
 sealed class LanguageEvent {}
 
 class ToggleLanguageEvent extends LanguageEvent {}
+
+class LanguageInitEvent extends LanguageEvent {}
