e8a24fa501ebff8c6cb05769346ad93854b074b9
Badrakova Valeria <lera_18a@mail.ru>
2025-10-20 13:58:16 +0300

    add provider

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,11 +1,14 @@
 import 'package:dio_http_example/bloc/bloc_holidays/holidays_bloc.dart';
 import 'package:dio_http_example/bloc/bloc_language/language_bloc.dart';
 import 'package:dio_http_example/bloc/bloc_theme/theme_bloc.dart';
+import 'package:dio_http_example/enviroment_variables.dart';
 import 'package:dio_http_example/extensions/build_context_extensions.dart';
+import 'package:dio_http_example/holidays_api_dio_client.dart';
 import 'package:dio_http_example/ui/ui_pages/splash_screen_page.dart';
 import 'package:dio_http_example/ui/theme/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'l10n/app_localizations.dart';
 
 class MyApp extends StatefulWidget {
@@ -18,29 +21,42 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => HolidaysBloc()),
-        BlocProvider(create: (context) => ThemeBloc()),
-        BlocProvider(create: (context) => LanguageBloc()),
-      ],
-      child: BlocBuilder<ThemeBloc, ThemeState>(
-        builder: (context, themState) {
-          return BlocBuilder<LanguageBloc, LanguageState>(
-            builder: (context, languageState) {
-              return MaterialApp(
-                locale: languageState.local,
-                localizationsDelegates: AppLocalizations.localizationsDelegates,
-                supportedLocales: AppLocalizations.supportedLocales,
-                darkTheme: darkTheme,
-                theme: lightTheme,
-                themeMode: themState.isLight ? ThemeMode.light : ThemeMode.dark,
-                onGenerateTitle: (context) => context.i18n.appTitle,
-                home: SplashScreen(),
-              );
-            },
-          );
-        },
+    return Provider<HolidaysApiDioClient>(
+      create: (_) => HolidaysApiDioClient(
+        apiKey: EnviromentVariables.apiKey,
+        host: EnviromentVariables.apiHost,
+      ),
+
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) =>
+                HolidaysBloc(apiClient: context.read<HolidaysApiDioClient>()),
+          ),
+          BlocProvider(create: (context) => ThemeBloc()),
+          BlocProvider(create: (context) => LanguageBloc()),
+        ],
+        child: BlocBuilder<ThemeBloc, ThemeState>(
+          builder: (context, themState) {
+            return BlocBuilder<LanguageBloc, LanguageState>(
+              builder: (context, languageState) {
+                return MaterialApp(
+                  locale: languageState.local,
+                  localizationsDelegates:
+                      AppLocalizations.localizationsDelegates,
+                  supportedLocales: AppLocalizations.supportedLocales,
+                  darkTheme: darkTheme,
+                  theme: lightTheme,
+                  themeMode: themState.isLight
+                      ? ThemeMode.light
+                      : ThemeMode.dark,
+                  onGenerateTitle: (context) => context.i18n.appTitle,
+                  home: SplashScreen(),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
